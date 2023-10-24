d1fa02e6f08bafcc9379baea2ff28fdec87288fe
Reiza <taimichael2@gmail.com>
2023-10-25 02:20:20 +0800

    feat: add admin view to routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { isAuthenticated } from '@/firebaseInit';
-// import Dashboard from '@/views/DashboardView.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import { isAuthenticated } from '@/firebaseInit'
+// import Home from '@/views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,28 +14,31 @@ const router = createRouter({
       path: '/boards/:boardId',
       name: 'board',
       component: () => import('@/views/BoardView.vue'),
-      props: route => ({ boardId: route.params.boardId }),
+      props: (route) => ({ boardId: route.params.boardId }),
     },
     {
       path: '/sign-in',
       name: 'sign-in',
       component: () => import('@/views/SignInView.vue'),
     },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('@/views/AdminView.vue'),
+    },
   ],
-});
+})
 
 router.beforeEach((to, from, next) => {
   if (to.path === '/sign-in' && isAuthenticated.value) {
-    next('/');
-    return;
+    next('/')
+    return
   }
 
   if (!isAuthenticated.value && to.path !== '/sign-in') {
-    next('/sign-in');
-    return;
+    next('/sign-in')
+    return
   }
+})
 
-  next();
-});
-
-export default router;
+export default router
