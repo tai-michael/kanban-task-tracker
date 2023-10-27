0793770d30d072b214de5a6136919746d33ff773
Reiza <taimichael2@gmail.com>
2023-10-27 22:27:58 +0800

    feat: add an unstyled Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li>{{ card.name }}</li>
+      <li>{{ card.due_date }}</li>
+      <li>{{ card.checklist_progress }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineAsyncComponent } from 'vue'
+const Card = defineAsyncComponent(() => import('@/components/Card.vue'))
+
+const props = defineProps(['card'])
+</script>
+
+<style scoped lang="scss">
+ul {
+  background: #73b2f1;
+  padding: 8px;
+}
+li {
+  font-style: italic;
+}
+</style>
