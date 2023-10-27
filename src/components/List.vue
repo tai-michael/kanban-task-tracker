4b36fa717781fd56544edc7b6fb9859f1e4a9a61
Reiza <taimichael2@gmail.com>
2023-10-27 22:27:23 +0800

    feat: add an unstyled List component

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ list.name }}</h1>
+
+    <div v-for="card of list.cards">
+      <Card :card="card"></Card>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineAsyncComponent } from 'vue'
+const Card = defineAsyncComponent(() => import('@/components/Card.vue'))
+
+const props = defineProps(['list'])
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-weight: 600;
+  font-size: 1rem;
+}
+</style>
