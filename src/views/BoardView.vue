25b5ae91f08007d4a4528024d284ba93c3b499bf
Reiza <taimichael2@gmail.com>
2023-11-02 20:24:57 +0800

    refactor: move fetching logic up a level to Board View

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,55 +1,81 @@
 <template>
   <div>
-    <Board
-      v-if="props.boardId || store.lastBoardRoute || card.boardId"
-      :boardId="boardId"
-    />
+    <div v-if="isFetchingBoard">Loading board...</div>
+    <Board v-if="boardStore.board" />
+    <!-- REVIEW could pass 'fetching' variables as props into their respective components and render spinners there; not sure which is the more reusable approach -->
 
     <transition name="fade">
       <div
-        v-if="cardId"
+        v-show="cardId"
         class="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center"
       >
-        <CardContent v-if="card" :card="card" />
+        <CardContent
+          v-show="cardStore.card"
+          @close-button-clicked="router.push(`/board/${boardStore.board.id}`)"
+          :isFetchingCard="isFetchingCard"
+        />
       </div>
     </transition>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, defineAsyncComponent, onMounted } from 'vue'
+import { ref, computed, defineAsyncComponent, onMounted, watch } from 'vue'
 import { db } from '@/firebaseInit'
 import { doc, getDoc } from 'firebase/firestore'
-import { useCardStore } from '@/stores'
-import { useRoute } from 'vue-router'
-const route = useRoute()
-const store = useCardStore()
+import { useCardStore, useBoardStore } from '@/stores'
+import { useRouter } from 'vue-router'
 const Board = defineAsyncComponent(() => import('@/components/Board.vue'))
 const CardContent = defineAsyncComponent(
   () => import('@/components/CardContent.vue')
 )
+const router = useRouter()
+const cardStore = useCardStore()
+const boardStore = useBoardStore()
 const props = defineProps(['cardId', 'boardId'])
 
-const boardId = computed(
-  () => props.boardId || store.lastBoardRoute || card.value.boardId
-)
+const boardId = computed(() => props.boardId || cardStore.card.boardId)
+
+const isFetchingBoard = ref(false)
+const fetchBoardData = async (id: string) => {
+  const boardRef = doc(db, 'boards_single', id)
+  const boardDoc = await getDoc(boardRef)
+  if (boardDoc.exists()) boardStore.hydrateBoard(boardDoc.data())
+  console.log(boardStore.board)
+}
 
 const card = ref({})
-const fetchingCardFromBackend = ref(false)
+const isFetchingCard = ref(false)
 const fetchCard = async (id: string) => {
   const cardRef = doc(db, 'cards', id)
   const cardDoc = await getDoc(cardRef)
-  console.log(cardDoc?.data()?.card)
-  if (cardDoc.exists()) return cardDoc.data()
+  if (cardDoc.exists()) cardStore.hydrateCard(cardDoc.data())
+  console.log(cardStore.card)
 }
 
-onMounted(async () => {
-  if (route.params.cardId) {
-    fetchingCardFromBackend.value = true
-    card.value = await fetchCard(props.cardId)
-    console.log(card.value)
-    fetchingCardFromBackend.value = false
-  } else if (route.params.boardId) {
+watch(
+  () => props.cardId,
+  async (newValue, oldValue) => {
+    if (newValue && newValue !== cardStore.card.id) {
+      console.log('new card id detected')
+      if (Object.keys(cardStore.card).length) cardStore.clearCard()
+      isFetchingCard.value = true
+      await fetchCard(props.cardId)
+      isFetchingCard.value = false
+    }
   }
+)
+
+onMounted(async () => {
+  console.log('Board View mounted')
+  if (props.cardId) {
+    isFetchingCard.value = true
+    await fetchCard(props.cardId)
+    isFetchingCard.value = false
+  }
+
+  isFetchingBoard.value = true
+  await fetchBoardData(boardId.value)
+  isFetchingBoard.value = false
 })
 </script>
