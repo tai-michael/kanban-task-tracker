998cee46012971ffd8fca7a7589d31357ae62899
Reiza <taimichael2@gmail.com>
2023-11-05 20:34:06 +0800

    feat: add card routing to card previews

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,18 +3,24 @@
     <h1>{{ list.name }}</h1>
 
     <div v-for="card of list.cards">
-      <CardPreview :card="card"></CardPreview>
+      <CardPreview
+        :card="card"
+        @click="router.push(`/card/${card.id}`)"
+      ></CardPreview>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, defineAsyncComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { useCardStore } from '@/stores'
 const CardPreview = defineAsyncComponent(
   () => import('@/components/CardPreview.vue')
 )
-
+const router = useRouter()
 const props = defineProps(['list'])
+const store = useCardStore()
 </script>
 
 <style scoped lang="scss">
