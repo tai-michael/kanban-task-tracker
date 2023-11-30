310205e34f3e5ea17d14b26f485288ce73f85955
Reiza <taimichael2@gmail.com>
2023-11-30 20:00:08 +0800

    refactor: change blur behavior for list composer

--- src/components/ListComposer.vue
+++ src/components/ListComposer.vue
@@ -1,26 +1,21 @@
 <template>
   <div class="min-w-[304px]">
-    <div v-if="isCreatingList">
+    <div v-if="isCreatingList" ref="composer">
       <div class="input-form">
         <input
           v-model="newListTitle"
           v-focus="isCreatingList"
-          @blur="processListCreation"
           @keyup.enter="processListCreation"
-          @keyup.esc="resetListCreationState"
+          @keyup.esc="hideListComposer"
           placeholder="Enter a list title"
           class="w-full"
         />
       </div>
       <div class="flex gap-x-3">
-        <button
-          :disabled="!newListTitle"
-          @mousedown="processListCreation"
-          class="border-2 pl-2 pr-2"
-        >
+        <button @mousedown="processListCreation" class="border-2 pl-2 pr-2">
           Save
         </button>
-        <button @mousedown="resetListCreationState" class="border-2 pl-2 pr-2">
+        <button @mousedown="hideListComposer" class="border-2 pl-2 pr-2">
           X
         </button>
       </div>
@@ -28,7 +23,7 @@
 
     <button
       v-else
-      @click.stop="isCreatingList = true"
+      @click.stop="showListComposer"
       class="w-full bg-green-400 ml-2 mr-2"
     >
       + Add a new list
@@ -37,22 +32,40 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref, type Ref } from 'vue'
 import createAndAddList from '@/composables/createAndAddList'
 import { useBoardStore } from '@/stores'
 const isCreatingList = ref(false)
 const newListTitle = ref('')
 const store = useBoardStore()
+const composer: Ref<HTMLElement | null> = ref(null)
 
 const processListCreation = () => {
-  if (newListTitle.value) createAndAddList(newListTitle.value)
-  resetListCreationState()
+  if (!newListTitle.value.trim()) return
+  createAndAddList(newListTitle.value)
+  newListTitle.value = ''
+  hideListComposer()
 }
 
-const resetListCreationState = () => {
-  newListTitle.value = ''
+const showListComposer = () => {
+  isCreatingList.value = true
+}
+
+const hideListComposer = () => {
   isCreatingList.value = false
 }
+
+const handleClickOutside = (e: MouseEvent) => {
+  if (composer.value && !composer.value.contains(e.target as Node))
+    hideListComposer()
+}
+
+onMounted(() => {
+  window.addEventListener('click', handleClickOutside)
+})
+onUnmounted(() => {
+  window.removeEventListener('click', handleClickOutside)
+})
 </script>
 
 <style scoped lang="scss">
