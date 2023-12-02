d8513712bc5cbc2af4873f0adaa37beacc295223
Reiza <taimichael2@gmail.com>
2023-12-03 03:50:58 +0800

    feat: adjust css of side panel

--- src/components/SidePanel.vue
+++ src/components/SidePanel.vue
@@ -3,11 +3,16 @@
     class="side-panel flex-shrink-0 flex-grow flex max-w-[200px] min-w-[150px]"
   >
     <div v-if="fetchingBoardsFromBackend">Loading...</div>
-    <div v-else>
-      <div v-for="board in store.boards" :key="board.id">
-        <router-link :to="`/board/${board.id}`">{{ board.title }}</router-link>
-      </div>
-    </div>
+    <ul v-else class="flex flex-col gap-y-[1px] p-1">
+      <li v-for="board in store.boards" :key="board.id" class="p-1">
+        <router-link :to="`/board/${board.id}`" class="flex w-full">{{
+          board.title
+        }}</router-link>
+      </li>
+      <button class="bg-green-400 rounded p-1 hover:bg-green-300">
+        + Create new board
+      </button>
+    </ul>
   </div>
 </template>
 
