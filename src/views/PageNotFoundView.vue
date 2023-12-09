70de255de6c8ce79354d5a86da127f9c7629fa69
Reiza <taimichael2@gmail.com>
2023-12-10 03:38:22 +0800

    feat: create rudimentary UI for Page Not Found component

--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="text-center">
+  <div class="text-center flex flex-col items-center p-2">
     <h1 class="font-bold text-2xl mb-4">Page not found</h1>
-    <span class="text-lg">{{ errMessage }}</span>
+    <span class="text-lg mb-4">{{ errMessage }}</span>
+    <button @click="router.push('/')" class="p-1.5 bg-blue-300 rounded">
+      Home
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, onUnmounted } from 'vue'
+import { useRouter } from 'vue-router'
 import { useErrorStore } from '@/stores'
+const router = useRouter()
 const store = useErrorStore()
 const errMessage = computed(
   () =>
