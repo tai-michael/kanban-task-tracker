05db418bad8ea7a9d71b65b794b2ae58d5264179
Reiza <taimichael2@gmail.com>
2024-01-08 04:31:16 +0800

    refactor: extrac hide sidebar button into own component

---
+++ src/components/HideSidebarButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button
+      class="w-full mt-2 p-3 pl-8 inline-flex items-center gap-4 hover-effect"
+      @mouseover="hovered = true"
+      @mouseleave="hovered = false"
+    >
+      <HideSidebarIcon
+        :color="`${hovered ? 'var(--main-purple)' : 'var(--medium-gray)'}`"
+      />
+      <span class="text-[var(--medium-gray)] font-bold">Hide Sidebar</span>
+    </button>
+    <div class="mt-3 ml-6">
+      <button class="mr-4" @click="signOut(auth)">Sign Out</button>
+      <router-link :to="`/admin`">Admin</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import HideSidebarIcon from '@/assets/icons/icon-hide-sidebar.vue'
+import { auth } from '@/firebaseInit'
+import { signOut } from 'firebase/auth'
+const hovered = ref(false)
+</script>
+
+<style scoped lang="scss">
+.hover-effect:hover {
+  background-color: var(--light-nav-hover);
+  border-radius: 0px 100px 100px 0px;
+
+  span {
+    color: var(--main-purple);
+  }
+}
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -14,27 +14,7 @@
 
     <div class="mt-auto flex flex-col">
       <DarkmodeToggle class="darkmode-toggle h-12 ml-6 mr-6" />
-      <!-- TODO Extract below into HideSidebarButton component -->
-      <div class="hide-sidebar-btn">
-        <button
-          class="w-full mt-2 p-3 pl-8 inline-flex items-center gap-4 hover-effect"
-          @mouseover="hideSidebarButtonHovered = true"
-          @mouseleave="hideSidebarButtonHovered = false"
-        >
-          <HideSidebarIcon
-            :color="`${
-              hideSidebarButtonHovered
-                ? 'var(--main-purple)'
-                : 'var(--medium-gray)'
-            }`"
-          />
-          <span class="text-[var(--medium-gray)] font-bold">Hide Sidebar</span>
-        </button>
-        <div class="mt-3 ml-6">
-          <button class="mr-4" @click="signOut(auth)">Sign Out</button>
-          <router-link :to="`/admin`">Admin</router-link>
-        </div>
-      </div>
+      <HideSidebarButton class="hide-sidebar-btn" />
     </div>
   </nav>
 </template>
@@ -44,57 +24,17 @@ import { defineAsyncComponent } from 'vue'
 import LogoDark from '@/assets/images/logo-dark.svg'
 import BoardSelector from '@/components/BoardSelector.vue'
 import DarkmodeToggle from '@/components/DarkmodeToggle.vue'
-import HideSidebarIcon from '@/assets/icons/icon-hide-sidebar.vue'
-import { auth } from '@/firebaseInit'
-import { signOut } from 'firebase/auth'
-import { useRoute, useRouter } from 'vue-router'
-const route = useRoute()
-const router = useRouter()
+import HideSidebarButton from '@/components/HideSidebarButton.vue'
+import { useRouter } from 'vue-router'
 import { useBoardStore } from '@/stores'
 const BoardComposer = defineAsyncComponent(
   () => import('@/components/BoardComposer.vue')
 )
+const router = useRouter()
 const store = useBoardStore()
-
-const hideSidebarButtonHovered = ref(false)
 </script>
 
 <style scoped lang="scss">
-.active-board {
-  background-color: var(--main-purple);
-  border-radius: 0px 100px 100px 0px;
-
-  img {
-    color: white;
-  }
-  span {
-    color: white;
-  }
-}
-
-
-.board-composer {
-  z-index: 1;
-  position: absolute;
-  top: 0; // Align top with Sidebar
-  left: 100%; // Position to the right of Sidebar
-  width: 300px;
-  height: 180px;
-  background-color: white; // Optional, for visibility
-  border: 1px solid #ccc; // Optional, for visibility
-  border-radius: 8px;
-}
-
-.logo,
-.hide-sidebar-btn {
-  display: none;
-}
-
-
-.darkmode-toggle {
-  margin: 1rem;
-}
-
 nav {
   position: fixed;
   top: 40%;
@@ -114,6 +54,27 @@ nav {
   nav {
     transition: opacity 0.5s, visibility 0.5s;
   }
+}
+
+.logo,
+.hide-sidebar-btn {
+  display: none;
+}
+
+.darkmode-toggle {
+  margin: 1rem;
+}
+
+.board-composer {
+  z-index: 1;
+  position: absolute;
+  top: 0; // Align top with Sidebar
+  left: 100%; // Position to the right of Sidebar
+  width: 300px;
+  height: 180px;
+  background-color: white; // Optional, for visibility
+  border: 1px solid #ccc; // Optional, for visibility
+  border-radius: 8px;
 }
 
 @media (min-width: 481px) {
