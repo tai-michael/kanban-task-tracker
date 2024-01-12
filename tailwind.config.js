e5cdb377a8d057c9e3151c9a61903b671a5ca486
Reiza <taimichael2@gmail.com>
2024-01-12 23:41:29 +0800

    feat: add custom mobile screensize to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      xs: '481px',
+      ...defaultTheme.screens,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
