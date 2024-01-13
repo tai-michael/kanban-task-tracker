65dd64a8d420983a9a2ef5b8ea515b3ad1af76cb
Reiza <taimichael2@gmail.com>
2024-01-14 05:02:00 +0800

    refactor: reorganize sidebar mobile and desktop views

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,104 +1,49 @@
 <template>
-  <nav class="flex flex-col">
-    <img
-      :src="LogoDark"
-      class="logo cursor-pointer w-[150px] ml-8 mt-8 mb-14"
-      @click="router.push('/')"
-    />
-
-    <BoardSelector />
-
-    <div v-if="store.isCreatingNewBoard" class="board-composer">
-      <BoardComposer />
+  <nav
+    class="flex flex-col shrink-0 xs:w-[var(--sidebar-width)] xs:border-r-[1px_solid_var(--lines-light)]"
+  >
+    <div
+      class="hidden xs:flex items-center shrink-0 h-[var(--header-height-desktop)] pl-8"
+    >
+      <img
+        :src="LogoDark"
+        class="cursor-pointer w-[150px]"
+        @click="router.push('/')"
+      />
     </div>
 
-    <div class="mt-auto flex flex-col">
-      <DarkmodeToggle class="darkmode-toggle h-12 ml-6 mr-6" />
-      <HideSidebarButton class="hide-sidebar-btn" />
+    <div class="overflow-auto relative pt-3.5">
+      <!-- <button
+        type="button"
+        @click="emit('closeClicked')"
+        class="absolute top-0 right-4 xs:hidden"
+      >
+        <img :src="CrossIcon" />
+      </button> -->
+      <BoardSelector
+        @board-link-clicked="emit('boardLinkClicked')"
+        @board-composer-triggered="emit('boardComposerTriggered')"
+      />
+    </div>
+    <div class="flex flex-col mt-auto xs:pt-3">
+      <DarkmodeToggle class="m-4 xs:my-0 xs:mx-6 h-12 ml-6 mr-6" />
+      <HideSidebarButton class="hidden xs:block xs:mr-6" />
     </div>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
 import LogoDark from '@/assets/images/logo-dark.svg'
 import BoardSelector from '@/components/BoardSelector.vue'
 import DarkmodeToggle from '@/components/DarkmodeToggle.vue'
 import HideSidebarButton from '@/components/HideSidebarButton.vue'
 import { useRouter } from 'vue-router'
-import { useBoardStore } from '@/stores'
-const BoardComposer = defineAsyncComponent(
-  () => import('@/components/BoardComposer.vue')
-)
 const router = useRouter()
-const store = useBoardStore()
+const emit = defineEmits([
+  'boardLinkClicked',
+  'boardComposerTriggered',
+  'closeClicked',
+])
 </script>
 
-<style scoped lang="scss">
-nav {
-  position: fixed;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 264px;
-  height: 322px;
-  background-color: white;
-  border-radius: 8px;
-
-  // padding: 5rem 6rem;
-  box-shadow: 0px 10px 20px 0px rgba(54, 78, 126, 0.25);
-  z-index: 2;
-}
-
-@media (max-width: 480px) {
-  nav {
-    transition: opacity 0.5s, visibility 0.5s;
-  }
-}
-
-.logo,
-.hide-sidebar-btn {
-  display: none;
-}
-
-.darkmode-toggle {
-  margin: 1rem;
-}
-
-.board-composer {
-  z-index: 1;
-  position: absolute;
-  top: 0; // Align top with Sidebar
-  left: 100%; // Position to the right of Sidebar
-  width: 300px;
-  height: 180px;
-  background-color: white; // Optional, for visibility
-  border: 1px solid #ccc; // Optional, for visibility
-  border-radius: 8px;
-}
-
-@media (min-width: 481px) {
-  nav {
-    position: relative;
-    top: unset;
-    left: unset;
-    transform: none;
-    width: 300px;
-    height: 100%;
-    flex-shrink: 0;
-    border-right: 1px solid var(--lines-light);
-    border-radius: 0;
-    box-shadow: none;
-  }
-
-  .logo,
-  .hide-sidebar-btn {
-    display: block;
-    margin-right: 1.5rem;
-  }
-
-  .darkmode-toggle {
-    margin: 0 1.5rem;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
