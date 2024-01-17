b2c45607d33dd88740d6b4574426c0583928ef2a
Reiza <taimichael2@gmail.com>
2024-01-17 23:23:32 +0800

    refactor: rename handler name in homepage greeting

--- src/components/HomePageGreeting.vue
+++ src/components/HomePageGreeting.vue
@@ -13,7 +13,7 @@
     <span>Welcome to Kanban! Get started with a board.</span>
 
     <button
-      @click="emit('boardComposerToggled')"
+      @click="emit('boardComposerTriggered')"
       class="flex justify-center items-center mt-6 py-3 gap-x-4 w-[var(--sidebar-navbtn-width-mobile)] xs:w-[var(--sidebar-navbtn-width-desktop)] bg-[var(--main-purple)] rounded-full hover:bg-[var(--main-purple-hover)]"
     >
       <BoardIcon :color="'white'" />
@@ -23,7 +23,7 @@
 
   <BoardSelector
     v-if="isMobileView && boardStore.boards?.length > 0"
-    @board-composer-triggered="emit('boardComposerToggled')"
+    @board-composer-triggered="emit('boardComposerTriggered')"
   />
 </template>
 
@@ -37,21 +37,5 @@ const BoardSelector = defineAsyncComponent(
   () => import('@/components/BoardSelector.vue')
 )
 defineProps(['isMobileView'])
-const emit = defineEmits(['boardComposerToggled'])
+const emit = defineEmits(['boardComposerTriggered'])
 </script>
-
-<style scoped lang="scss">
-// TODO maybe convert below to tailwind. Afterwards, redo the css of the board composer so that it doesn't look awful in desktop mode when there's no other board around. Consider hiding the sidebar when theres nothing
-.hover-effect {
-  background-color: var(--main-purple);
-  border-radius: 100px 100px 100px 100px;
-
-  span {
-    color: white;
-  }
-}
-
-.hover-effect:hover {
-  background-color: var(--main-purple-hover);
-}
-</style>
