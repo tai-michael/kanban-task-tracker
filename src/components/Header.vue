f360679774ae763cedd05984604703ee52b4046f
Reiza <taimichael2@gmail.com>
2024-01-22 03:33:19 +0800

    feat: add popover for ellipsis menu in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,11 +4,6 @@
       v-if="route.name === 'home'"
       class="flex justify-between gap-x-2 xs:hidden"
     >
-      <!-- <img
-        :src="LogoDark"
-        class="logo-desktop cursor-pointer w-[120px]"
-        @click="router.push('/')"
-      /> -->
       <div v-if="!searchStore.isSearching" class="flex items-center gap-x-2">
         <div class="flex w-10 h-10">
           <button class="icon-button">
@@ -18,7 +13,7 @@
         <span class="font-bold text-lg">Boards</span>
       </div>
 
-      <BoardSearch />
+      <BoardSearch v-if="boardStore.boards?.length > 0" />
     </div>
 
     <div
@@ -38,28 +33,37 @@
         @title-edited="changeBoardTitle"
         class="title"
       />
-      <!-- <button @click="emit('boardSelectorTriggered')" class="p-3">
-          <img :src="DownArrow" />
-        </button> -->
 
-      <div class="flex w-10 h-10">
-        <button @clicked="emit('ellipsisMenuTriggered')" class="icon-button">
+      <div class="flex w-10 h-10 relative">
+        <button @click="toggleEllipsisMenu" class="icon-button">
           <img
             :src="EllipsisIcon"
             class="w-[3.7px] h-[16px] xs:w-[5px] xs:h-[20px]"
           />
         </button>
-      </div>
 
-      <!-- TODO add popup for delete board -->
-      <!-- <button @click="handleDeleteBoard" class="p-1 bg-red-200">
-        Delete Board
-      </button> -->
+        <div v-if="isEllipsisMenuOpen">
+          <div class="backdrop" @click="toggleEllipsisMenu"></div>
+
+          <div class="ellipsis-popover">
+            <button
+              @click="handleDeleteBoard"
+              class="p-2 w-full bg-red-200 rounded-md"
+            >
+              Delete Board
+            </button>
+            <button class="p-2 w-full bg-red-200 rounded-md">
+              Change Title
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Title from '@/components/Title.vue'
 import BoardSearch from '@/components/BoardSearch.vue'
 import MenuIcon from '@/assets/icons/icon-menu.vue'
@@ -71,7 +75,7 @@ const route = useRoute()
 const router = useRouter()
 const boardStore = useBoardStore()
 const searchStore = useSearchStore()
-const emit = defineEmits(['boardSelectorTriggered', 'ellipsisMenuTriggered'])
+const emit = defineEmits(['boardSelectorTriggered'])
 
 const changeBoardTitle = (title: string) => {
   boardStore.updateBoardTitle(title)
@@ -82,8 +86,14 @@ const handleReturnToBoards = () => {
   boardStore.clearBoard()
 }
 
+const isEllipsisMenuOpen = ref(false)
+const toggleEllipsisMenu = () => {
+  isEllipsisMenuOpen.value = !isEllipsisMenuOpen.value
+}
+
 const handleDeleteBoard = () => {
   boardStore.deleteBoard()
+  toggleEllipsisMenu()
   // TODO add toast or something similar
 }
 </script>
@@ -112,15 +122,32 @@ header {
   width: 100%;
   height: 100%;
   border-radius: 9999px; /* fully rounded */
+.ellipsis-popover {
+  position: absolute;
+  top: 100%;
+  right: 0;
+  width: 200px;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  row-gap: 0.5rem;
+  padding: 1rem;
   justify-content: center;
-  transition: background-color 0.2s ease-in-out;
+  background-color: white;
+  border-radius: 0.375rem;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  z-index: 10;
 }
 
 .icon-button:hover,
 .icon-button:active {
   background-color: var(--header-button-hover);
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9;
 }
 
 @media (min-width: 481px) {
