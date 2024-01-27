b53f26aad5daa4c1cf2aa6f267d7f99c34ada200
Reiza <taimichael2@gmail.com>
2024-01-28 07:12:56 +0800

    feat: add fade transition component

---
+++ src/components/transitions/TransitionFade.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <transition name="fade" @enter="onEnter" @leave="onLeave"> -->
+  <!-- <transition name="fade" @enter="onEnter" mode="out-in"> -->
+  <transition name="fade" mode="out-in">
+    <slot></slot>
+  </transition>
+</template>
+
+<script setup>
+// const onEnter = (el, done) => {
+//   const delay = 2000 // Delay in milliseconds
+//   setTimeout(() => {
+//     el.style.opacity = 1
+//     done()
+//   }, delay)
+// }
+
+// onLeave()
+</script>
+
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+</style>
