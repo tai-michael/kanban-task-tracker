5e7dd154b497d1c4bf7b9cd3b80237d81d04a602
Reiza <taimichael2@gmail.com>
2024-02-05 00:00:54 +0800

    feat: enhance styling of the scrollbar in lists

--- src/components/List.vue
+++ src/components/List.vue
@@ -10,6 +10,7 @@
     </button>
   </div>
 
+  <!-- TODO place these inside computed -->
   <VueDraggable
     v-model="list.cards"
     group="cards"
@@ -22,10 +23,13 @@
     :scroll="true"
     :scroll-sensitivity="90"
     :bubble-scroll="true"
-    chosen-class="tilted"
+    :chosen-class="isMobileView ? 'tilted' : ''"
     drag-class="tilted"
     ghost-class="ghost"
     class="space-y-3 list-cards"
+    @move="checkIsCardHoveringOverDeleteZone"
+    @choose="store.setCardHoldingStatus(true)"
+    @unchoose="store.setCardHoldingStatus(false)"
   >
     <div v-for="card in list.cards" :key="card.id">
       <CardPreview
@@ -42,7 +46,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, inject, ref } from 'vue'
 import Title from '@/components/Title.vue'
 import { useRouter } from 'vue-router'
 import { useBoardStore } from '@/stores'
@@ -56,14 +60,25 @@ const CardComposer = defineAsyncComponent(
 const store = useBoardStore()
 const router = useRouter()
 const props = defineProps(['list'])
-
+const isMobileView = inject('isMobileView')
 const emit = defineEmits(['cardSelected'])
+
 const handleCardSelection = (cardId: string) => {
   emit('cardSelected')
   router.push(`/card/${cardId}`)
 }
+
 const changeListTitle = (title: string) => {
   store.updateListTitle(props.list.id, title)
+}
+
+const checkIsCardHoveringOverDeleteZone = (evt) => {
+  // console.log(evt.related)
+  if (evt.related && evt.related.classList.contains('delete-zone')) {
+    store.setCardHoverStatus(true)
+  } else {
+    store.setCardHoverStatus(false)
+  }
 }
 </script>
 
@@ -74,10 +89,6 @@ const changeListTitle = (title: string) => {
   overflow-y: auto;
   overflow-x: clip;
   max-height: calc(100svh - (var(--header-height-mobile) * 2 + 3rem));
-
-  @media (min-width: 481px) {
-    max-height: calc(100svh - (var(--header-height-desktop) * 2 + 1.5rem));
-  }
 }
 
 .tilted > * {
@@ -85,12 +96,40 @@ const changeListTitle = (title: string) => {
 }
 
 .ghost {
-  background-color: rgb(216, 216, 216);
+  // background-color: rgb(216, 216, 216);
   border-radius: 8px;
 }
 
 .ghost > * {
-  visibility: hidden;
-  // opacity: 0.2;
+  // visibility: hidden;
+  opacity: 0.2;
+}
+
+::-webkit-scrollbar {
+  width: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-track {
+  border-radius: 10px;
+}
+
+@media (min-width: 481px) {
+  .list-cards {
+    @media (min-width: 481px) {
+      max-height: calc(100svh - (var(--header-height-desktop) * 2 + 1.5rem));
+    }
+  }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #091e420f;
+  }
 }
 </style>
