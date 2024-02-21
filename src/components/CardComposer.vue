bc457b6785653676eec166983baf6f00b7647c54
Reiza <taimichael2@gmail.com>
2024-02-21 21:09:29 +0800

    feat: enhance styling of card composer

--- src/components/CardComposer.vue
+++ src/components/CardComposer.vue
@@ -7,21 +7,22 @@
       @keyup.enter="processCardCreation"
       @keyup.esc="resetCardCreationState"
       placeholder="Enter a card title"
-      class="w-full"
+      class="w-full resize-none focus:outline-none"
     />
   </div>
-  <div class="flex gap-x-3">
+
+  <div class="flex gap-x-1.5">
     <button
       @mousedown="processCardCreation"
       type="submit"
-      class="border-2 pl-2 pr-2"
+      class="px-4 rounded text-white bg-[var(--main-purple)] hover:bg-[var(--main-purple-hover)] active:bg-[var(--main-purple-hover)]"
     >
-      Save
+      Add card
     </button>
     <button
       @mousedown="resetCardCreationState"
       type="button"
-      class="border-2 pl-2 pr-2"
+      class="p-2.5 rounded hover:bg-[var(--icon-button-hover)] active:bg-[var(--icon-button-active)]"
     >
       X
     </button>
@@ -31,9 +32,9 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import createAndAddCard from '@/composables/createAndAddCard'
+// TODO add 'required' to all required props in project
 const props = defineProps(['listId', 'isCreatingCard'])
 const emit = defineEmits(['exitCardComposer'])
-// TODO add 'required' to all required props in project
 const newCardTitle = ref('')
 
 // extra flag needed to prevent blur handler from triggering unnecessarily; props do not 'refresh' until the function has completed, so using props.isCreatingCard as the flag does not work
@@ -67,8 +68,8 @@ onMounted(() => {
   display: flex;
   padding: 12px;
   margin-bottom: 8px;
-  background: #73b2f1;
-  border: black 1px solid;
+  background: white;
+  box-shadow: var(--box-shadow-light);
   border-radius: 8px;
   min-height: 24px;
 }
