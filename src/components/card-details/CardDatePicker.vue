ce5b533e02bb8667dda58967be7c414ca583d04a
Reiza <taimichael2@gmail.com>
2024-02-27 22:11:09 +0800

    feat: further enhance styling of datepicker calender

--- src/components/card-details/CardDatePicker.vue
+++ src/components/card-details/CardDatePicker.vue
@@ -1,97 +1,97 @@
 <template>
-  <div class="mb-3">
-    <label class="block mb-2 text-sm font-semibold text-[var(--dark-gray-blue)]"
-      >Due date:</label
+  <label class="block mb-2 text-sm font-semibold text-[var(--card-text-subtle)]"
+    >Due date:</label
+  >
+
+  <div class="flex gap-x-2">
+    <input
+      v-if="selectedDate"
+      type="checkbox"
+      :checked="store.cardSummary?.is_completed"
+      @click="store.toggleCardCompleted(store.cardSummary.id)"
+      :title="
+        store.cardSummary?.is_completed
+          ? 'This card is complete.'
+          : 'This card is past due.'
+      "
+    />
+
+    <div
+      class="backdrop"
+      v-if="isMobileView && isDatePickerOpen"
+      @wheel.prevent
+      @touchmove.prevent
+      @scroll.prevent
+    ></div>
+
+    <VueDatePicker
+      ref="datePicker"
+      v-model="selectedDate"
+      @open="toggleBackdrop"
+      @closed="toggleBackdrop"
+      @update:model-value="handleDateSelected"
+      time-picker-inline
+      select-text="Save"
+      :teleport-center="isMobileView ? true : false"
+      :alt-position="customCalenderPosition"
+      :dark="isDark ? true : false"
     >
+      <template #trigger>
+        <input
+          :value="formattedDueDate"
+          type="text"
+          readonly
+          placeholder="Select Date"
+          class="w-[130px] py-1 px-3 border-2 cursor-pointer"
+          :class="{ 'pl-5': !selectedDate }"
+        />
+      </template>
 
-    <div class="flex gap-x-2">
-      <input
-        v-if="selectedDate"
-        type="checkbox"
-        :checked="store.cardSummary?.is_completed"
-        @click="store.toggleCardCompleted(store.cardSummary.id)"
-        :title="
-          store.cardSummary?.is_completed
-            ? 'This card is complete.'
-            : 'This card is past due.'
-        "
-      />
+      <template #action-buttons class="relative z-[var(--z-popover)]">
+        <span
+          class="absolute top-[13px] left-[43%] font-semibold text-[var(--card-text-subtle)]"
+          >Dates</span
+        >
 
-      <div
-        class="backdrop"
-        v-if="isMobileView && isDatePickerOpen"
-        @wheel.prevent
-        @touchmove.prevent
-        @scroll.prevent
-      ></div>
+        <button
+          @click.prevent="datePicker?.closeMenu"
+          type="button"
+          class="absolute top-[8px] right-2 font-normal p-2.5 rounded hover:bg-[#f3f3f3] active:bg-[#f3f3f3]"
+        >
+          <img :src="CrossIcon" class="w-3 h-3" />
+        </button>
 
-      <VueDatePicker
-        ref="datePicker"
-        v-model="selectedDate"
-        @open="toggleBackdrop"
-        @closed="toggleBackdrop"
-        @update:model-value="handleDateSelected"
-        time-picker-inline
-        select-text="Save"
-        :teleport-center="isMobileView ? true : false"
-        :alt-position="customCalenderPosition"
-        :dark="isDark ? true : false"
-      >
-        <template #trigger>
-          <input
-            :value="formattedDueDate"
-            type="text"
-            readonly
-            placeholder="Select Date"
-            class="w-[120px] py-1 px-2 border-2 cursor-pointer"
-          />
-        </template>
-
-        <template #action-buttons class="relative z-[var(--z-popover)]">
-          <span class="absolute top-[12px] left-[43%] font-semibold"
-            >Dates</span
-          >
-
+        <div class="my-0.5">
           <button
-            @click.prevent="datePicker?.closeMenu"
+            v-if="selectedDate"
             type="button"
-            class="absolute top-[8px] right-2 font-normal p-2.5 rounded hover:bg-[#f3f3f3] active:bg-[#f3f3f3]"
+            class="w-[65px] h-[35px] mr-1 rounded hover:bg-[#f3f3f3] active:bg-[#f3f3f3]"
+            @click.prevent="handleClearDueDate"
           >
-            <img :src="CrossIcon" class="w-3 h-3" />
+            Remove
           </button>
+          <button
+            type="button"
+            class="w-[65px] h-[35px] rounded text-white bg-[#1976d2] hover:bg-[#1c8cfc] active:bg-[#1c8cfc]"
+            @click.prevent="datePicker?.selectDate"
+          >
+            Save
+          </button>
+        </div>
+      </template>
+    </VueDatePicker>
 
-          <div class="my-0.5">
-            <button
-              v-if="selectedDate"
-              type="button"
-              class="w-[65px] h-[35px] mr-1 rounded hover:bg-[#f3f3f3] active:bg-[#f3f3f3]"
-              @click.prevent="handleClearDueDate"
-            >
-              Remove
-            </button>
-            <button
-              type="button"
-              class="w-[65px] h-[35px] rounded text-white bg-[#1976d2] hover:bg-[#1c8cfc] active:bg-[#1c8cfc]"
-              @click.prevent="datePicker?.selectDate"
-            >
-              Save
-            </button>
-          </div>
-        </template>
-      </VueDatePicker>
-
-      <div class="flex items-center justify-center">
-        <span
-          v-if="isCompleted"
-          class="bg-[#1f845a] text-white px-1.5 flex items-center rounded h-[85%]"
-          >Completed</span
-        >
-        <span
-          v-if="isOverdue"
-          class="bg-[#ffeceb] text-[#ae2a19] px-1.5 flex items-center rounded h-[87%]"
-          >Overdue</span
-        >
-      </div>
+    <div class="flex items-center justify-center">
+      <span
+        v-if="isCompleted"
+        class="bg-[#1f845a] text-white px-1.5 flex items-center rounded h-[85%]"
+        >Completed</span
+      >
+      <span
+        v-if="isOverdue"
+        class="bg-[#ffeceb] text-[#ae2a19] px-1.5 flex items-center rounded h-[87%]"
+        >Overdue</span
+      >
     </div>
   </div>
 </template>
@@ -107,7 +107,7 @@ import { useCardStore } from '@/stores'
 const store = useCardStore()
 const isDark = useDark()
 const isMobileView = inject('isMobileView')
-const customCalenderPosition = () => ({ top: 44, left: -30 })
+const customCalenderPosition = () => ({ top: 43, left: 0 })
 
 const isDatePickerOpen = ref(false)
 const toggleBackdrop = () => {
@@ -166,9 +166,9 @@ const isOverdue = computed(() => {
 
 <style scoped lang="scss">
 .dp__main {
-  width: 120px !important;
+  width: 130px !important;
   --dp-menu-min-width: 300px;
-  --dp-menu-padding: 40px 10px 0 10px;
+  --dp-menu-padding: 50px 10px 0 10px;
   --dp-font-family: var(--font-family);
   --dp-font-size: 14px;
 }
