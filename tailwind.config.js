8d236673a87101a353c42f9fb3184fa1960efa53
Reiza <taimichael2@gmail.com>
2024-03-06 22:16:46 +0800

    feat: reconfigure tailwind for darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // 'media' or 'class'
   theme: {
     extend: {},
     screens: {
