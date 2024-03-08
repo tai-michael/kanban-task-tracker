8af63cd3c78305101d46b6ae23aeed05d2f78c02
Reiza <taimichael2@gmail.com>
2024-03-08 18:44:25 +0800

    feat: add darkmode colors to delete confirmation

--- src/components/DeleteConfirmation.vue
+++ src/components/DeleteConfirmation.vue
@@ -11,14 +11,14 @@
     <button
       @click="emit('cancelTriggered')"
       type="button"
-      class="modal-button text-[var(--main-purple)] bg-[var(--main-purple-transparent)] hover:bg-[var(--icon-button-hover)] active:bg-[var(--icon-button-active)] transition-colors duration-100 ease-in-out"
+      class="modal-button text-[13px] text-[var(--main-purple)] font-bold bg-[var(--main-purple-transparent)] dark:hover:bg-[white] hover:bg-[var(--icon-button-hover)] active:bg-[var(--icon-button-active)] transition-colors duration-100 ease-in-out"
     >
       <slot name="cancel-button">Cancel</slot>
     </button>
     <button
       @click="emit('deleteTriggered')"
       type="button"
-      class="modal-button text-white bg-red-500 hover:bg-red-400 active:bg-red-300 transition-colors duration-100 ease-in-out"
+      class="modal-button text-[13px] text-white font-bold bg-red-500 hover:bg-red-400 active:bg-red-300 transition-colors duration-100 ease-in-out"
     >
       <slot name="delete-button">Delete</slot>
     </button>
