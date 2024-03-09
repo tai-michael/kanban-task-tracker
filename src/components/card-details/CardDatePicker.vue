cc841de2419b83f76906abec8096838fb4a324ff
Reiza <taimichael2@gmail.com>
2024-03-09 12:33:42 +0800

    feat: add darkmode colors to card date picker

--- src/components/card-details/CardDatePicker.vue
+++ src/components/card-details/CardDatePicker.vue
@@ -1,5 +1,6 @@
 <template>
-  <label class="block mb-2 text-sm font-semibold text-[var(--card-text-subtle)]"
+  <label
+    class="block mb-2 text-sm font-semibold text-[var(--card-text-subtle)] dark:text-white"
     >Due date</label
   >
 
